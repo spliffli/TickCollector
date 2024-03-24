6eea69de826a077c0338c7838acff90e889e8ef5
Jonathon <jonathon@spliff.li>
2024-03-24 09:22:06 +0100

    Updated filepath for mt4ConfigExample.ini in Program.cs

--- TickCollector/Program.cs
+++ TickCollector/Program.cs
@@ -8,7 +8,7 @@ class Program
     static void Main(string[] args)
     {
         var logger = new FileLogger("log.txt", logToConsole: true);
-        var config = new MTConfiguration("C:\\Users\\Jonathon\\source\\repos\\TickCollectionEngine\\TickCollectionEngine\\mt4ConfigExample.ini");
+        var config = new MTConfiguration("C:\\Users\\Jonathon\\source\\repos\\TickCollector\\TickCollector\\mt4ConfigExample.ini");
         var mt4EventHandler = new MTEventHandler(config, logger);
         var fpMarketsLiveSessionController = new MTSessionController(config, mt4EventHandler, logger);
 
