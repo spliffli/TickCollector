ae5ad8c51338c8d9a3fc4d24a85c210aeb35e85f
Jonathon <jonathon@spliff.li>
2024-03-24 09:17:04 +0100

    Add project files.

---
+++ TickCollector/Program.cs
@@ -0,0 +1,19 @@
+﻿using TradingApis;
+using TradingApis.Common.Loggers;
+using TradingApis.MetaTrader;
+
+namespace TickCollector;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var logger = new FileLogger("log.txt", logToConsole: true);
+        var config = new MTConfiguration("C:\\Users\\Jonathon\\source\\repos\\TickCollectionEngine\\TickCollectionEngine\\mt4ConfigExample.ini");
+        var mt4EventHandler = new MTEventHandler(config, logger);
+        var fpMarketsLiveSessionController = new MTSessionController(config, mt4EventHandler, logger);
+
+
+        var tickCollector = new TickCollector(fpMarketsLiveSessionController, logger);
+        tickCollector.Start();
+    }
+}
---
+++ TickCollector/TickCollector.cs
@@ -0,0 +1,60 @@
+﻿using TradingApis;
+using TradingApis.Common;
+using TradingApis.Common.Loggers;
+
+namespace TickCollector;
+
+internal class TickCollector
+{
+    private readonly Logger _logger;
+    private readonly IList<TradingSessionController> _sessionControllers;
+
+    private TickCollector()
+    {
+        _logger = new ConsoleLogger();
+        _logger.Log("TickCollector initialized without passing a Logger, so defaulting to ConsoleLogger.");
+
+        _sessionControllers = null!; 
+    }
+
+    private TickCollector(Logger logger)
+    {
+        _logger = logger;
+
+        _sessionControllers = null!;
+    }
+
+    public TickCollector(IList<TradingSessionController> sessionControllers)
+        : this()
+    {
+        _sessionControllers = sessionControllers;
+    }
+
+    public TickCollector(IList<TradingSessionController> sessionControllers, Logger logger)
+        : this(logger)
+    {
+        _sessionControllers = sessionControllers;
+    }
+
+    public TickCollector(TradingSessionController sessionController)
+        : this()
+    {
+        var sessionControllers = new List<TradingSessionController>();
+        sessionControllers.Add(sessionController);
+        _sessionControllers = sessionControllers;
+    }
+
+    public TickCollector(TradingSessionController sessionController, Logger logger)
+        : this(sessionController)
+    {
+        _logger = logger;
+    }
+
+    public void Start()
+    {
+        foreach (var sessionController in _sessionControllers)
+        {
+            sessionController.StartThread();
+        }
+    }
+}
